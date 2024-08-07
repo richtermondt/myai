90b63dc62979f1da35287590106eabfa8e6f5d24
Rich Termondt <rtermondt@gmail.com>
2024-08-07 12:16:17 -0500

    iteration

--- app/handler/openai_streamer.py
+++ app/handler/openai_streamer.py
@@ -1,5 +1,4 @@
 from openai import OpenAI
-import os
 
 class OpenAIStreamer:
     def __init__(self):
@@ -9,15 +8,20 @@ class OpenAIStreamer:
     def chat(self, message):
         if message:
             self.messages.append({"role": "user", "content": message})
-            stream = self.client.chat.completions.create(model="gpt-3.5-turbo", messages=self.messages, stream=True)
+            stream = self.client.chat.completions.create(
+                model="gpt-3.5-turbo", 
+                messages=self.messages, 
+                stream=True
+            )
             for chunk in stream:
                 if not chunk.choices:
                     continue
                 if chunk.choices[0].delta.content is not None:
-                    print(chunk.choices[0].delta.content, end="")
-            print()
+                    yield(chunk.choices[0].delta.content)
+                    #print(chunk.choices[0].delta.content, end="")
+            #print()
 
-def main():
+if __name__ == "__main__":
     openai_streamer = OpenAIStreamer()
     while True:
         message = input("User : ")
