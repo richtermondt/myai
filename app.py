c11ff076b753d2ee7900408a6943bf9d261df0c2
Rich Termondt <rtermondt@gmail.com>
2024-08-20 09:59:28 -0500

    Dockerized app and tested build and run via docker.

--- app.py
+++ app.py
@@ -7,4 +7,6 @@ load_dotenv('.flaskenv')
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_RUN_PORT", 5000))
+    app.run(host=host, port=port, debug=True)
